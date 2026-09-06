72a8470a96ab80f94d02b03e787fa88667908f92
pmganga <p.mganga@alustudent.com>
2026-09-06 12:22:37 +0300

    Add 3-two.py

---
+++ 3-two.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python3
+"""
+Plots two line graphs showing the exponential decay of C-14 and Ra-226.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 21000, 1000)
+r = np.log(0.5)
+t1 = 5730
+t2 = 1600
+y1 = np.exp((r / t1) * x)
+y2 = np.exp((r / t2) * x)
+
+# Plot C14 as red line & Ra226 as green line
+plt.plot(x, y1, 'r--', label='C-14')
+plt.plot(x, y2, 'g-', label='Ra-226')
+
+plt.title("Exponential Decay of Radioactive Elements")
+plt.xlabel("Time (years)")
+plt.ylabel("Fraction Remaining")
+
+# x & y axis limits
+plt.xlim(0, 20000)
+plt.ylim(0, 1)
+
+plt.legend(loc='upper right')
+
+plt.show()
