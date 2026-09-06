948cd6d72b7696bbd64c192634871f9de3ae14a2
pmganga <p.mganga@alustudent.com>
2026-09-06 12:13:10 +0300

    Update 2-change_scale.py

--- 2-change_scale.py
+++ 2-change_scale.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""
+Plots a line graph showing the exponential decay of C-14.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 28650, 5730)
+x = np.arange(0, 28650)
 r = np.log(0.5)
 t = 5730
 y = np.exp((r / t) * x)
