5872c75b0dd7b1f478618b5586fb505d1dcd732e
pmganga <p.mganga@alustudent.com>
2026-09-13 17:20:28 +0300

    Fix Task 11: Remove numpy import and use object method

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """Module that transposes a numpy ndarray."""
-import numpy as np
 
 
 def np_transpose(matrix):
     """Transposes a numpy ndarray.
 
     Args:
-        matrix: A numpy.ndarray or array-like object.
+        matrix: A numpy.ndarray.
 
     Returns:
         A new transposed numpy.ndarray.
     """
-    return np.transpose(matrix)
+    return matrix.transpose()
+    
