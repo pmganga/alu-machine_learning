70d8dacc98f234fa55121c1711d7a2f8760e2186
pmganga <p.mganga@alustudent.com>
2026-09-16 13:36:18 +0300

    Add 0-determinant.py

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Module to calculate the determinant of a matrix."""
+
+
+def determinant(matrix):
+    """Calculates the determinant of a matrix."""
+    if type(matrix) is not list or not all(type(row) is list for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        return 1
+
+    n = len(matrix)
+    if not all(len(row) == n for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+
+    if n == 1:
+        return matrix[0][0]
+
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for c in range(n):
+        sub_matrix = [row[:c] + row[c+1:] for row in matrix[1:]]
+        det += ((-1) ** c) * matrix[0][c] * determinant(sub_matrix)
+
+    return det
